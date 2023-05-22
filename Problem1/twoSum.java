d7edfd2a6f0f20b21bd6f91f7541edecf957334d
Caleb St.Germain <98179578+Calebstg@users.noreply.github.com>
2023-05-22 15:22:00 +0000

    Added already completed problems in java

---
+++ Problem1/twoSum.java
@@ -0,0 +1,15 @@
+/* Author: Caleb St.Germain
+ * Date: 5/22/2023
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[j] == target - nums[i]){
+                    return new int[] {i,j};
+                }
+            }
+            }
+            return null;
+        }
+    }
